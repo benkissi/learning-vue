d3c2a612f7a5d6543f447d5dc693ac329be697cd
benkissi <benkissi111@gmail.com>
2020-01-12 12:23:42 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,32 @@
+var app = new Vue({
+  el: "#app",
+  data: {
+    title: "Becoming a vue ninja",
+    name: "Ben Kissi",
+    url: "https://youtube.com",
+    classes: ["one", "two"],
+    wage: 10,
+    coords: {
+      x: 0,
+      y: 0
+    }
+  },
+  methods: {
+    greet: function(time) {
+      return `Hello and good ${time}, ${this.name}`;
+    },
+    changeWage: function(amount) {
+      return (this.wage += amount);
+    },
+    logEvent: function(e) {
+      console.log(e);
+    },
+    logCoords: function(e) {
+      this.coords.x = e.offsetX;
+      this.coords.y = e.offsetY;
+    },
+    updateName: function(e) {
+      this.name = e.target.value;
+    }
+  }
+});
